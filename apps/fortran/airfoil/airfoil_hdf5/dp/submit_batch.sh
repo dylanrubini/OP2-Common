904332f3aa8765a9e9eff8ca802832a83208bfca
dylanrubini <dylan.rubini@hotmai.co.uk>
2021-06-01 12:21:19 +0100

    fix

--- apps/fortran/airfoil/airfoil_hdf5/dp/submit_batch.sh
+++ apps/fortran/airfoil/airfoil_hdf5/dp/submit_batch.sh
@@ -19,4 +19,4 @@ module load ParMETIS/4.0.3-iimpi-2020a
 module load HDF5/1.10.5-iimpi-2020a
 module load Valgrind/3.16.1-iimpi-2020a
 
-mpirun $MPI_HOSTS ./airfoil_hdf5_mpi.x
+mpirun $MPI_HOSTS ./airfoil_hdf5_mpi
